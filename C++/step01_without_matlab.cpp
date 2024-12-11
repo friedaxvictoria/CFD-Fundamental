716cc23965772dcf43924e207cf94e7707ad1475
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2024-12-11 09:14:54 +0100

    print

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -59,7 +59,7 @@ int main() {
         std::copy(std::begin(u), std::end(u), std::begin(un));
         //un = u;
 
-        //std::cout << n << "\n" << std::endl;
+        std::cout << n << "\n" << std::endl;
 
         #pragma omp parallel for simd
             for (int i = 1; i < X; i++) {
