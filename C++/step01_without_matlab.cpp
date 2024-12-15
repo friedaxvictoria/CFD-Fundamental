903183f5e4422483f5431c466972f3ab7e7058fd
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2024-12-15 15:27:51 +0100

    loop

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -18,7 +18,6 @@ namespace plt = matplotlibcpp;
 ////////////////////////////////////////////////////////////
 
 int main() {
-    auto start = high_resolution_clock::now();
     // Simulation parameters
     //const int X = 40;
     //const int T = 41;
@@ -34,73 +33,75 @@ int main() {
     double x[X], u[X], un[X];
     double testun[X], testu[X];
 
-    std::cout << "first loop" << std::endl;
+    sum_values = 0;
+    num_rounds = 10;
 
-    #pragma omp parallel for simd 
-        for (int i = 0; i < X; i++) {
-            x[i] = (5.0 * i) / (X - 1);
-            u[i] = (x[i] >= 0.5 && x[i] <= 1) ? 2 : 1;
-            if (i % 10000 == 0) {
-                std::cout << i << "\n" << std::endl;
+    for (int round = 0; round < num_rounds; round++) {
+
+        auto start = high_resolution_clock::now();
+
+        #pragma omp parallel for simd 
+            for (int i = 0; i < X; i++) {
+                x[i] = (5.0 * i) / (X - 1);
+                u[i] = (x[i] >= 0.5 && x[i] <= 1) ? 2 : 1;
             }
-        }
 
 
-    //std::copy(std::begin(testu), std::end(testu), std::begin(u));
+        //std::copy(std::begin(testu), std::end(testu), std::begin(u));
 
-#ifdef MATPLOTLIB
-    plt::ion();
-    plt::Plot plot;
-#endif
-    // Time-stepping loop
-    std::cout << "second loop" << std::endl;
+        #ifdef MATPLOTLIB
+            plt::ion();
+            plt::Plot plot;
+        #endif
+            // Time-stepping loop
+            //std::cout << "second loop" << std::endl;
 
-    
-    for (int n = 0; n < T; n++) {
-        #pragma omp for simd
-        for (int i = 1; i < X; i++)
-            un[i] = u[i];
-        //std::copy(std::begin(u), std::end(u), std::begin(un));
-        //un = u;
-        #pragma omp parallel for simd
-            for (int i = 1; i < X; i++) {
-                u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
+
+            for (int n = 0; n < T; n++) {
+                #pragma omp for simd
+                std::copy(std::begin(u), std::end(u), std::begin(un));
+                //un = u;
+                #pragma omp parallel for simd
+                for (int i = 1; i < X; i++) {
+                    u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
+                }
+
+
+        #ifdef MATPLOTLIB
+                plot.update(x, u);
+                plt::xlim(0, 2);
+                plt::ylim(0.5, 2.5);
+                plt::pause(0.1);
+        #endif
             }
-    
-    
-#ifdef MATPLOTLIB
-        plot.update(x, u);
-        plt::xlim(0, 2);
-        plt::ylim(0.5, 2.5);
-        plt::pause(0.1);
-#endif
-    }
 
-    /*
-    for (int n = 0; n < T; n++) {
-        std::copy(std::begin(testu), std::end(testu), std::begin(testun));
-        for (int i = 1; i < X; i++) {
-            testu[i] = testun[i] - c * (testun[i] - testun[i - 1]) * dt / dx;
+            /*
+            for (int n = 0; n < T; n++) {
+                std::copy(std::begin(testu), std::end(testu), std::begin(testun));
+                for (int i = 1; i < X; i++) {
+                    testu[i] = testun[i] - c * (testun[i] - testun[i - 1]) * dt / dx;
+                }
+            }
+
+            for (int i = 0; i < X; i++) {
+                std::cout << u[i] << std::endl;
+                std::cout << testu[i] << std::endl;
+                if (u[i] != testu[i])
+                    std::cout << "u is unequal" << std::endl;
+            }
+             */
+
+            auto stop = high_resolution_clock::now();
+            auto duration = duration_cast<microseconds>(stop - start);
+            std::cout << "microseconds: " << duration.count() << std::endl;
+            auto duration_sec = duration_cast<seconds>(stop - start);
+            std::cout << "seconds: " << duration_sec.count() << std::endl;
+            sum_values += duration.count();
+        #ifdef MATPLOTLIB
+            plt::show();
+        #endif
         }
-    }
-
-    for (int i = 0; i < X; i++) {
-        std::cout << u[i] << std::endl;
-        std::cout << testu[i] << std::endl;
-        if (u[i] != testu[i])
-            std::cout << "u is unequal" << std::endl;
-    }
-     */   
-    
-
-#ifdef MATPLOTLIB
-    plt::show();
-#endif
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    std::cout << "microseconds: " << duration.count() << std::endl;
-    auto duration_sec = duration_cast<seconds>(stop - start);
-    std::cout << "seconds: " << duration_sec.count() << std::endl;
+    std::cout << "average microseconds: " << sum_values/num_rounds << std::endl;
 
     return 0;
 }
