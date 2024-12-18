d4a0da8efe882ae218b85019a7a5fcd3750b1e38
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2024-12-18 08:01:07 +0100

    with simd

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -43,7 +43,7 @@ int main() {
 
             //not good with simd bc of implied if statement? should you split into two loops?
             #ifdef PARALLEL
-            #pragma omp parallel for
+            #pragma omp parallel for simd
             for (int i = 0; i < X; i++) {
                 x[i] = (5.0 * i) / (X - 1);
             }
@@ -56,7 +56,7 @@ int main() {
             for (int n = 0; n < T; n++) {
                 std::copy(std::begin(u), std::end(u), std::begin(un));
                 //un = u;
-                #pragma omp parallel for
+                #pragma omp parallel for simd
                 for (int i = 1; i < X; i++) {
                     u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
                 }
