6afa3774eeac6c82292795bfed02c7ec94bf085e
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2024-12-22 12:17:52 +0100

    clean up

--- C++/step05_without_matlab.cpp
+++ C++/step05_without_matlab.cpp
@@ -10,10 +10,10 @@ using namespace std::chrono;
 // Step 5: 2D Linear Convection
 ////////////////////////////////////////////////////////////
 
-const int X = 31;                         // Number of points along X-axis
-const int Y = 31;                         // Number of points along Y-axis
+const int X = 300;                         // Number of points along X-axis
+const int Y = 300;                         // Number of points along Y-axis
 static float x[X], y[X];
-static float nX[X][Y], nY[X][Y], u[X][Y], un[X][Y], u_not[X][Y];
+static float nX[X][Y], nY[X][Y], u[X][Y], un[X][Y];
 
 //std::vector<std::vector<double>> u(X, std::vector<double>(Y)), un(X, std::vector<double>(Y));
 //std::vector<std::vector<double>> nX(X, std::vector<double>(Y)), nY(X, std::vector<double>(Y));
@@ -32,20 +32,20 @@ int main() {
     int num_rounds = 1;
 
 
-    for (int round = 0; round < num_rounds; round++) {
+for (int round = 0; round < num_rounds; round++) {
     auto start = high_resolution_clock::now();
 
-#ifdef PARALLEL
+    #ifdef PARALLEL
     // Create spatial grids
-#pragma omp parallel for simd
+    #pragma omp parallel for simd
     for (int i = 0; i < X; i++)
         x[i] = (2 * i) / (X - 1.0);
 
-#pragma omp parallel for simd
+    #pragma omp parallel for simd
     for (int i = 0; i < Y; i++)
         y[i] = (2 * i) / (Y - 1.0);
 
-#pragma omp parallel for simd collapse(2) 
+    #pragma omp parallel for simd collapse(2) 
     for (int i = 0; i < X; ++i) {
         for (int j = 0; j < Y; ++j) {
             nX[i][j] = x[i];
@@ -54,7 +54,7 @@ int main() {
     }
 
     // no simd bc of i-else
-#pragma omp parallel for collapse(2) 
+    #pragma omp parallel for collapse(2) 
     for (int i = 0; i < X; i++) {
         for (int j = 0; j < Y; j++)
             u[i][j] = ((x[i] >= 0.5 && x[i] <= 1) && (y[j] >= 0.5 && y[j] <= 1)) ? 2.0 : 1.0;
@@ -66,70 +66,25 @@ int main() {
         std::copy(&u[0][0], &u[0][0] + X * Y, &un[0][0]);
         //std::copy(std::begin(u), std::end(u), std::begin(un));
 
-#pragma omp parallel for simd collapse(2) 
+        #pragma omp parallel for simd collapse(2) 
         for (int i = 1; i < X - 1; i++) {
             for (int j = 1; j < Y - 1; j++)
                 u[i][j] = un[i][j] - c * (un[i][j] - un[i - 1][j]) * dt / dx - c * (un[i][j] - un[i][j - 1]) * dt / dx;
         }
 
         // Boundary conditions
-#pragma omp parallel for simd
+        #pragma omp parallel for simd
         for (int i = 0; i < Y; i++) u[i][0] = 1.;
-#pragma omp parallel for simd
+        #pragma omp parallel for simd
         for (int i = 0; i < X; i++) u[0][i] = 1.;
 
-#pragma omp parallel for simd
+        #pragma omp parallel for simd
         for (int i = 0; i < X; i++) u[i][Y - 1] = 1.;
-#pragma omp parallel for simd
+        #pragma omp parallel for simd
         for (int i = 0; i < Y; i++) u[X - 1][i] = 1.;
     }
 
-        //not parallel
-        for (int i = 0; i < X; i++)
-            x[i] = (2 * i) / (X - 1.0);
-
-        for (int i = 0; i < Y; i++)
-            y[i] = (2 * i) / (Y - 1.0);
-
-        for (int i = 0; i < X; ++i) {
-            for (int j = 0; j < Y; ++j) {
-                nX[i][j] = x[i];
-                nY[i][j] = y[j];
-            }
-        }
-
-        for (int i = 0; i < X; i++) {
-            for (int j = 0; j < Y; j++)
-                u_not[i][j] = ((x[i] >= 0.5 && x[i] <= 1) && (y[j] >= 0.5 && y[j] <= 1)) ? 2.0 : 1.0;
-        }
-
-
-        // Time-stepping loop
-        for (int n = 0; n < T; n++) {
-            std::copy(&u_not[0][0], &u_not[0][0] + X * Y, &un[0][0]);
-
-            for (int i = 1; i < X - 1; i++) {
-                for (int j = 1; j < Y - 1; j++)
-                    u_not[i][j] = un[i][j] - c * (un[i][j] - un[i - 1][j]) * dt / dx - c * (un[i][j] - un[i][j - 1]) * dt / dx;
-            }
-
-            // Boundary conditions
-            for (int i = 0; i < Y; i++) u_not[i][0] = 1.;
-            for (int i = 0; i < X; i++) u_not[0][i] = 1.;
-
-            for (int i = 0; i < X; i++) u_not[i][Y - 1] = 1.;
-            for (int i = 0; i < Y; i++) u_not[X - 1][i] = 1.;
-        }
-
-        for (int i = 0; i < X; i++) {
-            for (int j = 0; j < Y; j++)
-                if (u_not[i][j] != u[i][j]) {
-                    std::cout << u_not[i][j] << std::endl;
-                    std::cout << u[i][j] << std::endl;
-                    std::cout << "\n" << std::endl;
-                }
-        }
-#else
+    #else
     // Create spatial grids
     for (int i = 0; i < X; i++)
         x[i] = (2 * i) / (X - 1.0);
@@ -166,14 +121,14 @@ int main() {
         for (int i = 0; i < X; i++) u[i][Y - 1] = 1.;
         for (int i = 0; i < Y; i++) u[X - 1][i] = 1.;
     }
-#endif
+    #endif
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     std::cout << "microseconds: " << duration.count() << std::endl;
     auto duration_sec = duration_cast<seconds>(stop - start);
     std::cout << "seconds: " << duration_sec.count() << std::endl;
     sum_values += duration.count();
-        }
-        std::cout << "average microseconds: " << sum_values / num_rounds << std::endl;
+    }
+    std::cout << "average microseconds: " << sum_values / num_rounds << std::endl;
     return 0;
 }
