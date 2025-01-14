d554889a0620bf1a8c35a6039468a47e44f2ee82
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2025-01-14 11:34:58 +0100

    test pointers

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -17,11 +17,15 @@ namespace plt = matplotlibcpp;
 // Step 1: 1D Linear Convection
 ////////////////////////////////////////////////////////////
 const int X = 200000000;                    // Number of spatial points
-static float x[X], u[X], un[X];
+//static float x[X], u[X], un[X];
 int main() {
     // Simulation parameters
     //const int X = 40;
     //const int T = 41;
+    float *x = (float *) malloc(sizeof(float)*X);
+    float *u = (float *) malloc(sizeof(float)*X);
+    float *un = (float *) malloc(sizeof(float)*X);
+    //float (*x) = malloc (sizeof(*array) * X);
     const int T = 200;                    // Number of time steps
     const int c = 1;                     // Wave speed
 
@@ -57,8 +61,9 @@ int main() {
             }
 
             for (int n = 0; n < T; n++) {
-                std::copy(std::begin(u), std::end(u), std::begin(un));
+                //std::copy(std::begin(u), std::end(u), std::begin(un));
                 //un = u;
+                memcpy(un, u, sizeof(float) * X);
                 #pragma omp parallel for simd schedule(static, X/64)
                 for (int i = 1; i < X; i++) {
                     u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
@@ -70,8 +75,9 @@ int main() {
                 u[i] = (x[i] >= 0.5 && x[i] <= 1) ? 2 : 1;
             }
             for (int n = 0; n < T; n++) {
-                std::copy(std::begin(u), std::end(u), std::begin(un));
+                //std::copy(std::begin(u), std::end(u), std::begin(un));
                 //un = u;
+                memcpy(un, u, sizeof(float) * X);
                 for (int i = 1; i < X; i++) {
                     u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
                 }
