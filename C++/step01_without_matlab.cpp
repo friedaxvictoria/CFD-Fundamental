50d3b13fa2dbcce6b022a618ccb7913f39c4e9ea
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2025-01-14 16:36:55 +0100

    t

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -68,13 +68,6 @@ int main() {
                 u[i] = (x[i] >= 0.5 && x[i] <= 1) ? 2 : 1;
             }
 
-            for (int i = 0; i < X; i++) {
-                std::cout << u[i] << std::endl;
-                std::cout << u2[i] << std::endl;
-                if (u[i] != u2[i])
-                    std::cout << "u is unequal" << std::endl;
-            }
-
             for (int n = 0; n < T; n++) {
                 //std::copy(std::begin(u), std::end(u), std::begin(un));
                 //std::copy(u, u + X, un);
@@ -85,9 +78,6 @@ int main() {
                 for (int i = 1; i < X; i++) {
                     u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
                 }
-        
-
-
             }
 
             // Time-stepping loop
@@ -99,8 +89,10 @@ int main() {
                 }
             }
             for (int i = 0; i < X; i++) {
-                x2[i] = (5.0 * i) / (X - 1);
-                u2[i] = (x2[i] >= 0.5 && x2[i] <= 1) ? 2 : 1;
+                std::cout << u[i] << std::endl;
+                std::cout << u2[i] << std::endl;
+                if (u[i] != u2[i])
+                    std::cout << "u is unequal" << std::endl;
             }
             #else
             for (int i = 0; i < X; i++) {
