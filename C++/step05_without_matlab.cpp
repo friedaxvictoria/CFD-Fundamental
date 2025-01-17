b51ac5ad20d84283c51785776de7893ccb4890bd
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2025-01-17 08:28:40 +0100

    test

--- C++/step05_without_matlab.cpp
+++ C++/step05_without_matlab.cpp
@@ -115,53 +115,6 @@ for (int round = 0; round < num_rounds; round++) {
         u[X*(X - 1)+i] = 1.;
     } */
 
-
-        std::vector<double> x2(X), y2(Y);
-
-    for (int i = 0; i < X; i++)
-        x2[i] = (2 * i) / (X - 1.0);
-
-    for (int i = 0; i < Y; i++)
-        y2[i] = (2 * i) / (Y - 1.0);
-
-    std::vector<std::vector<double>> nX2(X, std::vector<double>(Y)), nY2(X, std::vector<double>(Y));
-    for (int i = 0; i < X; ++i) {
-        for (int j = 0; j < Y; ++j) {
-            nX2[i][j] = x2[i]; 
-            nY2[i][j] = y2[j]; 
-        }
-    }
-
-    std::vector<std::vector<double>> u2(X, std::vector<double>(Y)), un2(X, std::vector<double>(Y));
-    for (int i = 0; i < X; i++){
-        for (int j = 0; j < Y; j++)
-            u2[i][j] = ((x2[i] >= 0.5 && x2[i] <= 1) && (y2[j] >= 0.5 && y2[j] <= 1)) ? 2.0 : 1.0;        
-    }
-
- 
-    // Time-stepping loop
-    for (int n = 0; n < T; n++) {
-        un2 = u2;
-
-        for (int i = 1; i < X-1; i++) {
-            for (int j = 1; j < Y-1; j++)
-            u2[i][j] = un2[i][j] - c * (un2[i][j] - un2[i-1][j]) * dt / dx - c * (un2[i][j] - un2[i][j-1]) * dt / dx;
-        }
-
-        // Boundary conditions
-        for (int i = 0; i < Y; i++) u2[i][0] = 1.;
-        for (int i = 0; i < X; i++) u2[0][i] = 1.;
-
-        for (int i = 0; i < X; i++) u2[i][Y-1] = 1.;
-        for (int i = 0; i < Y; i++) u2[X-1][i] = 1.;
-    }
-    for (int i = 0; i < X; i++){
-        for (int j = 0; j < Y; j++){
-            std::cout<<u2[i][j]<<std::endl;  
-            int idx = i*X+j;  
-            std::cout<<u[idx]<<std::endl; 
-        }           
-    }
     #else
     // Create spatial grids
     for (int i = 0; i < X; i++)
@@ -185,6 +138,8 @@ for (int round = 0; round < num_rounds; round++) {
         }
     }
 
+    for (int i = 0; i < X; i++) un[i*X] = u[i*X];
+    for (int i = 0; i < Y; i++) un[i] = u[i*X];
 
     // Time-stepping loop
     for (int n = 0; n < T; n++) {
@@ -192,25 +147,12 @@ for (int round = 0; round < num_rounds; round++) {
         un = u;
         u = tmp;
 
-        for (int i = 0; i < X; i++) u[i*X] = un[i*X];
-        for (int i = 0; i < Y; i++) u[i] = un[i*X];
-
         for (int i = 1; i < X - 1; i++) {
             for (int j = 1; j < Y - 1; j++){
                 int idx = i*X+j;
                 u[idx] = un[idx] - c * (un[idx] - un[(i-1)*X+j]) * dt / dx - c * (un[idx] - un[idx-1]) * dt / dx;
             }
         }
-
-        // Boundary conditions
-        for (int i = 0; i < X; i++) {
-            u[i*X] = 1.;
-            u[i*X+(Y - 1)] = 1.;
-        }
-        for (int i = 0; i < Y; i++){
-            u[i] = 1.;
-            u[X*(X - 1)+i] = 1.;
-        } 
     }
     #endif
     auto stop = high_resolution_clock::now();
