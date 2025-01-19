9e9441f3446366db20972bd1446f8331a1a93eb3
friedaxvictoria <61278471+friedaxvictoria@users.noreply.github.com>
2025-01-19 17:51:31 +0100

    without simd

--- C++/step01_without_matlab.cpp
+++ C++/step01_without_matlab.cpp
@@ -52,7 +52,7 @@ int main() {
         auto start = high_resolution_clock::now();
 
             #ifdef PARALLEL
-            #pragma omp simd
+            #pragma omp parallel for
             for (int i = 0; i < X; i++) {
                 x[i] = (5.0 * i) / (X - 1);
             }
@@ -68,7 +68,7 @@ int main() {
                 un = u;
                 u = tmp;
                 
-                #pragma omp simd
+                #pragma omp parallel for schedule(static, chunk_size)
                 for (int i = 1; i < X; i++) {
                     u[i] = un[i] - c * (un[i] - un[i - 1]) * dt / dx;
                 }
